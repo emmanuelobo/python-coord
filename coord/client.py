91381f334545fed6a26e60d766e1b89201d11b51
= <emmaoobo@gmail.com>
2018-04-10 23:26:33 -0400

    setup init

---
+++ coord/client.py
@@ -0,0 +1,8 @@
+
+class Coord:
+
+	BASE_URL = ''
+	endpoint = {}
+
+	def __init__(self, secret_key):
+		self.secret_key = secret_key
---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+	name='python-coord',
+	version='0.1dev',
+	packages=['coord', ],
+)
