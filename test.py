41dbf439cddb45bce61521fa2c8f532600355b77
= <emmaoobo@gmail.com>
2018-04-12 02:59:47 -0400

    test

---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+import requests
+
+
+class BikeTests(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def test_base_url_endpoint(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
