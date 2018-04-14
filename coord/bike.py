e281eb0986943bf926a9e5148bee2d6c8df76e28
= <emmaoobo@gmail.com>
2018-04-14 17:11:01 -0400

    rental quote info

--- coord/bike.py
+++ coord/bike.py
@@ -3,6 +3,10 @@ import requests
 
 
 class Bike(BaseAPI):
+	"""
+	The Bike Share API is a comprehensive API that allows both getting information about bike share systems,
+	including available bikes and prices, as well as actually booking bikes from those systems.
+	"""
 
 	def __init__(self, *args):
 		super(Bike, self).__init__(*args)
@@ -13,11 +17,12 @@ class Bike(BaseAPI):
 		Get a list of locations given the input parameters.
 		Specify a search area by radius around a latitude and longitude, as well as any filter for specific systems.
 		Each location will be a GeoJSON Feature, and aggregated into a GeoJSON FeatureCollection.
+
 		:param latitude: float
 		:param longitude: float
 		:param radius_km: float
-		:param system_ids: int
-		:return:
+		:param system_ids: array
+		:return: dict
 		"""
 
 		path = f'{self.BIKE_PATH}location?latitude={latitude}&longitude={longitude}&radius_km={radius_km}&{self.secret_key}'
@@ -28,16 +33,29 @@ class Bike(BaseAPI):
 		"""
 		A bike location may be a single bike station (which can have multiple docked bikes) or a single dockless bike itself.
 		All working docks are returned, but only free and rentable dockless bikes are returned.
+
 		:param location_id: int
 		:param system_id: int
 		:return: dict
 		"""
-		path = f'{self.BIKE_PATH}location?{system_id}&{location_id}'
+		path = f'{self.BIKE_PATH}location/{system_id}&{location_id}?{self.secret_key}'
 		response = requests.get(path)
 		return response.json()
 
-	def rental_quote_info(self):
-		pass
+	def rental_quote_info(self, system_ids=None):
+		"""
+		Provide quotes for renting a bike, via different types of passes that you can buy.
+		Quotes can be obtained for all systems or for specific systems.
+		Quotes may be associated with specific regions within a system.
+		Please keep in mind that both the quotes and usage fees associated with the quotes are estimates.
+
+		:param system_ids: array
+		:return: dict
+		"""
+
+		path = f'{self.BIKE_PATH}quote?{self.secret_key}'
+		response = requests.get(path)
+		return response.json()
 
 	def system_info(self):
 		pass
