6057b7b74d0085eefc2efab162603c41762d0c7a
= <emmaoobo@gmail.com>
2018-04-23 23:20:01 -0400

    curb test case

--- tests/test_curb.py
+++ tests/test_curb.py
@@ -13,7 +13,7 @@ class CurbsTest(TestCase):
 		self.api_key = config('API_KEY')
 		self.curbs_api = Curb(self.api_key)
 
-	@vcr.use_cassette('fixtures/vcr_cassettes/test_base_api.yaml', record_mode='all')
+	@vcr.use_cassette('./fixtures/vcr_cassettes/test_base_api.yaml', record_mode='all')
 	def test_base_api(self):
 		url = f'https://api.coord.co/v1/search/curbs/bybounds/all_rules?min_latitude=44.444&max_latitude=44.444&min_longitude=44.444&max_longitude=44.444&access_key={self.api_key}'
 		actualResponse = requests.get(url).json()
